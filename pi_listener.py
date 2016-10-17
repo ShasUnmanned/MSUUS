67ff4de50c930466a50fe08b1f311135d17eaa37
salvi <salvi@broad.msu.edu>
2016-10-17 15:33:12 -0400

    started work on run_command function

--- pi_listener.py
+++ pi_listener.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
- 
+
+@app.route('/run_command')
+def run_command(command, options):
+    subprocess.call([command, options])
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
