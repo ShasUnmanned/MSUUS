03a40302b45c4730a2b9620e285a65009bfb8d60
salvi <salvi@broad.msu.edu>
2016-10-17 16:20:19 -0400

    made changes to run_command.py

--- pi_listener.py
+++ pi_listener.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/run_command')
+# run a command passed by POST method as a shell command on the pi
+# example would  be like http://serverurl:5000/run_command?command=something&options=somethingelse
+# might not work and might need to be passed as a json object
+@app.route('/run_command', methods=['POST'])
 def run_command(command, options):
-    subprocess.call([command, options])
+    subprocess.call([request.form['command'], request.form['options']])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
