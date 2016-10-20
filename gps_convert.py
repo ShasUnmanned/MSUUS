fe4e0647792f8cc9e157e734876fde94f63800fe
p4nicdrunk <salvijor@msu.edu>
2016-10-20 16:10:19 -0400

    changes to gps_convert.py

--- gps_convert.py
+++ gps_convert.py
@@ -2,8 +2,10 @@ import sys
 from LatLon import *
 
 def main(argv):
+	print(argv)
+	print(argv[0])
 	output_str = string2latlon(argv[0], argv[1], 'd% %m% %S% H')
-	print(output_str)
+	print(output_str.to_string('d%_%M'))
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
