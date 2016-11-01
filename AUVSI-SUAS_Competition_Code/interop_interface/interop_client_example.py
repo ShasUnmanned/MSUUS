69a8f59b38b13950dd1dec9940f974f780fb28a7
p4nicdrunk <salvijor@msu.edu>
2016-11-01 17:07:09 -0400

    working on client example

--- AUVSI-SUAS_Competition_Code/interop_interface/interop_client_example.py
+++ AUVSI-SUAS_Competition_Code/interop_interface/interop_client_example.py
@@ -7,7 +7,7 @@ from Tkinter import *
 
 def main():
 	telemetry_open = False
-	client = interop.Client(url="",username="",password="")
+	client = interop.Client(url='http://127.0.0.1:8000', username='testuser', password='testpass')
 	
 	def upload_telemetry(client, out):
                 telemetry = interop.Telemetry(latitude=38.145215,
@@ -16,40 +16,37 @@ def main():
 			uas_heading=90)
                 #send that info to the interop server
                 client.post_telemetry(telemetry)
-		out.insert("Telemetry posted")
+		out.insert(END,"Telemetry posted")
+
+	def upload_target(client, out):
+		try:
+                        #create a target object. we will be building this objec$
+                        #the output of our image classification program, from v$
+                        #stored in our database.
+                        target = interop.Target(type='standard',
+                                                latitude=38.145215,
+                                                longitude=-76.427942,
+                                                orientation='n',
+                                                shape='square',
+                                                background_color='green',
+                                                alphanumeric='A',
+                                                alphanumeric_color='white')
+                        #send the target info to the interop server
+                        target = client.post_target(target)
+                        out.insert(END, client.get_obstacles())
+		except:
+			out.insert(END, 'something went wrong when uploading target')
+
 
 	def connect(url, username, password, out):
-		#set up the connection to the interop server at the specified
-		#url with the specified username/password
-		client = interop.Client(url=url,
-		                        username=username,
-		                        password=password)
-	
-		#create an object with telemetry info (we will be getting the
-		#telemetry info from the autopilot so this code would have to
-		#run on the GCS machine as a MissionPlanner script)
-		telemetry = interop.Telemetry(latitude=38.145215,
-		                              longitude=-76.427942,
-		                              altitude_msl=50,
-		                              uas_heading=90)
-		#send that info to the interop server
-		client.post_telemetry(telemetry)
-		
-		#create a target object. we will be building this object using 
-		#the output of our image classification program, from values
-		#stored in our database.
-		target = interop.Target(type='standard',
-		                        latitude=38.145215,
-		                        longitude=-76.427942,
-		                        orientation='n',
-		                        shape='square',
-		                        background_color='green',
-		                        alphanumeric='A',
-		                        alphanumeric_color='white')
-		#send the target info to the interop server
-		target = client.post_target(target)
-		out.insert(END, client.get_obstacles())
-		
+		try:
+			#set up the connection to the interop server at the specified
+			#url with the specified username/password
+			client = interop.Client(url=url,
+			                        username=username,
+			                        password=password)
+		except:
+			out.insert(END, "something when wrong when trying to connect")	
 
 	def telemetry_tab():
 		#if (telemetry_open == False):
@@ -61,7 +58,7 @@ def main():
 
 	window = Tkinter.Tk()
 	window.title("MSUUS")
-	window.geometry("640x640")
+	window.geometry("660x500")
 
         url = StringVar( window )
         url.set('http://127.0.0.1:8000')
@@ -91,11 +88,17 @@ def main():
 	output_textbox.grid(row=5,column=1)
 
         connect_button = Button( window, text="Connect", command = lambda: connect(url.get(),username.get(),password.get(),output_textbox) )
-        connect_button.grid(row=3,column=1)
+        connect_button.grid(row=3,column=0)
 
 
 	telemetry_tab_button = Button( window, text="Telemetry", command = telemetry_tab() )
-	
+	#telemetry_tab_button.grid(row=3,column=2)
+
+
+	target_upload_button = Button( window, text="Upload Target", command = lambda: upload_target(client, output_textbox) )
+	target_upload_button.grid(row=3,column=1)
+
+
 	#url_label.pack()
 	#url_textbox.pack()
 	#username_label.pack()
