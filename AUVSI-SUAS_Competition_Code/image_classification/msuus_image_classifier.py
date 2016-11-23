8d1e95aabcf0fcd5f3faa4968a8375307574361b
salvi <salvijor@msu.edu>
2016-11-22 23:52:09 -0500

    more changes to nn arch

--- AUVSI-SUAS_Competition_Code/image_classification/msuus_image_classifier.py
+++ AUVSI-SUAS_Competition_Code/image_classification/msuus_image_classifier.py
@@ -12,9 +12,9 @@ from PIL import Image
 from PIL import ImageFilter
 # load dataset of auvsi targets
 # or generate them on demand here??
-num_variations = 16
+num_variations = 1
 num_training_images = int(26*13)*num_variations
-num_testing_images = 16*num_variations
+num_testing_images = 32*num_variations
 images = [None] * num_training_images
 labels = [None] * num_training_images
 images_test = [None] * num_testing_images
@@ -37,28 +37,32 @@ for i in range(0, 26):
 		
 		for q in range(0, num_variations):
 			tmp_img_2 = tmp_img
+			tmp_img_2 = tmp_img_2.filter(ImageFilter.FIND_EDGES)
+			tmp_img_2 = tmp_img_2.filter(ImageFilter.SMOOTH)
+			tmp_img_2 = tmp_img_2.filter(ImageFilter.SMOOTH_MORE)
+			tmp_img_2 = tmp_img_2.filter(ImageFilter.FIND_EDGES)
 			tmp_img_2 = tmp_img_2.convert('L')
-			tmp_img_2 = tmp_img_2.filter(ImageFilter.EDGE_ENHANCE)
-			tmp_img_2 = tmp_img_2.filter(ImageFilter.CONTOUR)
-			tmp_img_2 = tmp_img_2.filter(ImageFilter.EDGE_ENHANCE)
+			tmp_img_2 = tmp_img_2.filter(ImageFilter.EDGE_ENHANCE_MORE)
 			images[counter] = np.reshape(tmp_img_2.getdata(), (64, 64, -1))
 			labels[counter] = np.reshape(tmp_label, (-1))
 			ls_str = 'letter ' + letter_list[i]
 			print(ls_str + ", shape " + shape_list[a] + ' variation ' + str(q))
 
 			counter += 1
-
+		#tmp_img_2.show()
 
 for i in range(0, num_testing_images):
 	tmp_img, tmp_label = target_gen.generate_image(return_type = "set")
+	tmp_img = tmp_img.filter(ImageFilter.FIND_EDGES)
+	tmp_img = tmp_img.filter(ImageFilter.SMOOTH)
+	tmp_img = tmp_img.filter(ImageFilter.SMOOTH_MORE)
+	tmp_img = tmp_img.filter(ImageFilter.FIND_EDGES)
 	tmp_img = tmp_img.convert('L')
-	tmp_img = tmp_img.filter(ImageFilter.EDGE_ENHANCE)
-	tmp_img = tmp_img.filter(ImageFilter.CONTOUR)
-	tmp_img = tmp_img.filter(ImageFilter.EDGE_ENHANCE)
+	tmp_img = tmp_img.filter(ImageFilter.EDGE_ENHANCE_MORE)
 	images_test[i] = np.reshape(tmp_img.getdata(), (64, 64, -1))
 	labels_test[i] = np.reshape(tmp_label, (-1))
 	print("generating testing image " + str(i+1) + "/" + str(num_testing_images))
-
+#tmp_img.show()
 
 # shuffle images
 images, labels = shuffle(images, labels)
@@ -74,8 +78,8 @@ img_distortion = ImageAugmentation()
 # only flip left/right for shape training
 #img_distortion.add_random_flip_leftright()
 
-img_distortion.add_random_rotation(max_angle=360.)
-img_distortion.add_random_blur(sigma_max=3.)
+#img_distortion.add_random_rotation(max_angle=360)
+#img_distortion.add_random_blur(sigma_max=3.)
 
 
 
@@ -88,7 +92,7 @@ network = input_data(shape=[None, 64, 64, 1],
 	data_augmentation=img_distortion)
 
 # convolution 
-network = conv_2d(network, 128, 8, activation='relu')
+network = conv_2d(network, 64, 8, activation='relu')
 
 # max pooling
 network = max_pool_2d(network, 2)
@@ -97,13 +101,13 @@ network = max_pool_2d(network, 2)
 network = conv_2d(network, 64, 4, activation='relu')
 
 # convolution 3
-#network = conv_2d(network, 32, 2, activation='relu')
+network = conv_2d(network, 32, 8, activation='relu')
 
 # max pooling 2
 network = max_pool_2d(network, 2)
 
 # fully-connected
-network = fully_connected(network, 128, activation='relu')
+network = fully_connected(network, 64, activation='relu')
 
 # dropout
 network = dropout(network, 0.5)
@@ -115,12 +119,12 @@ network = fully_connected(network, 2, activation='softmax')
 network = regression(network, optimizer='adam', loss='categorical_crossentropy', learning_rate=0.001)
 
 
-model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path='/media/salvi/E4D81381D81350E2/checkpoints/msuus-target-classifier.tfl.ckpt')
+model = tflearn.DNN(network, tensorboard_verbose=2, checkpoint_path='/home/salvi/checkpoints/msuus-target-classifier.tfl.ckpt')
 
 # if previously trained model is available use that
 #model.load('msuus-target-classifier.tfl')
 
-model.fit(images, labels, n_epoch=100, shuffle=True, validation_set=(images_test, labels_test), show_metric=True, batch_size=64, snapshot_epoch=True, run_id='msuus-target-classifier')
+model.fit(images, labels, n_epoch=100, shuffle=True, validation_set=(images_test, labels_test), show_metric=True, batch_size=32, snapshot_epoch=True, run_id='msuus-target-classifier')
 
 model.save("msuus-target-classifier.tfl")
 print("Network trained and saved as msuus-target-classifier.tfl")
