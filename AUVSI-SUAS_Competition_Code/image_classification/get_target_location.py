c9f99624976081ab1ce983671dab3894c8963912
p4nicdrunk <salvijor@msu.edu>
2017-02-10 00:28:39 -0500

    get_target_location should be finished

--- AUVSI-SUAS_Competition_Code/image_classification/get_target_location.py
+++ AUVSI-SUAS_Competition_Code/image_classification/get_target_location.py
@@ -1,6 +1,7 @@
 import sys, math
+from math import sin, cos
 
-def get_location(x, y, fov, altitude, gps):
+def get_location(x, y, fov, altitude, gps, heading):
 	""" Takes an x,y coordinate of a target in an image, and uses the 
 	current altitude, gps coordinates, and camera field of view to 
 	calculate the gps position of the target. """
@@ -8,7 +9,8 @@ def get_location(x, y, fov, altitude, gps):
 	dx = x*(fov[0]*altitude) # x distance from image center in meters
 	dy = y*(fov[1]*altitude) # y distance from image center in meters
 	
-	### still need to rotate dx,dy by the current drone orientation ###	
+	dx = dx*cos(heading)-dy*sin(heading) # rotate x based on heading
+	dy = dy*cos(heading)+dx*sin(heading) # rotate x based on heading
 	
 	lat = gps[0] + (180/pi)*(dy/6378137)
 	lon = gps[1] + (180/pi)*(dx/6378137)/cos(gps[1])
